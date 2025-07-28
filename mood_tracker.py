74ea5803dc860f59a1e873c3e65f50ff1f06119e
strawberryganache <aleena.mohammed10@gmail.com>
2025-07-28 11:09:35 -0400

    Update mood_tracker.py - change plt.style.use

--- mood_tracker.py
+++ mood_tracker.py
@@ -51,7 +51,7 @@ if st.checkbox("Show mood chart"):
         scores = [x[1] for x in data]
         
         plt.clf()
-        plt.style.use('bmh')
+        plt.style.use('classic')
         plt.figure(figsize=(10, 6))
         plt.plot(dates, scores, marker='o', color='mediumseagreen')
         plt.xticks(rotation=45)
